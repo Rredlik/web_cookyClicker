7715500bb7151fa6f6cd4b01a3b4c73b230b3cbd
Rredlik <rredlik@mail.ru>
2022-05-25 23:15:28 +0500

    Отображение Заданий и Core в админской панели

--- users/admin.py
+++ users/admin.py
@@ -2,7 +2,12 @@ from django.contrib import admin
 from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
 
+from users.models import Task, Core
+
 User = get_user_model()
+
+admin.site.register(Task)
+admin.site.register(Core)
 
 
 @admin.register(User)
